9abc5b01334f14b789e4db56bceddfd23d944116
peter <niqixian@gmail.com>
2024-04-15 19:31:24 -0500

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
 end
